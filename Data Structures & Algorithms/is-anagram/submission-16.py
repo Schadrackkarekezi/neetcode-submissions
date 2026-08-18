3424d628f96d8c1cce25dd9550907c53331083f5
Schadrack Karekezi <vitalshadrackk@gmail.com>
2026-08-18 16:10:49 -0700

    Add: is-anagram - submission-16

---
+++ Data Structures & Algorithms/is-anagram/submission-16.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counts = {}
+        if len(s) != len(t):
+            return False
+
+        for i in range(len(s)):
+            counts[s[i]] = 1 + counts.get(s[i], 0)
+        
+        for i in range(len(t)):
+            if t[i] not in counts:
+                return False
+        return True
+
+        
