56dfb50b8318353e8c1baf0ae40d24f008762046
Schadrack Karekezi <vitalshadrackk@gmail.com>
2026-08-20 09:50:35 -0700

    Add: anagram-groups - submission-27

---
+++ Data Structures & Algorithms/anagram-groups/submission-27.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+        for word in strs:
+            key = tuple(sorted(word))
+            if key not in anagrams:
+                anagrams[key] = []
+            anagrams[key].append([word])
+        return List(anagrams.values())
+   
+        
+
+        
+
+
+
