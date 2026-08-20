01d69640a54ff877c42a7fbade97bc823c718831
Schadrack Karekezi <vitalshadrackk@gmail.com>
2026-08-20 13:27:04 -0700

    Add: is-anagram - submission-24

---
+++ Data Structures & Algorithms/is-anagram/submission-24.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        counterT, counterS = {},{}
+        if len(t) != len(s):
+            return False
+        for i in range(len(s)):
+            counterS[s[i]] = 1 + counterT.get(s[i], 0)
+            counterS[t[i]] = 1 + counterT.get(t[i], 0)
+
+        return counterS == counterT 
+
+
+
+            
+            
+
+
+        
