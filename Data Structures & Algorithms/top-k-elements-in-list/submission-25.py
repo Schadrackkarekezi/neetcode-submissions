06f0a8548a1459fbea13023502ba40c2b1c05511
Schadrack Karekezi <vitalshadrackk@gmail.com>
2026-08-31 14:11:04 -0700

    Add: top-k-elements-in-list - submission-25

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-25.py
@@ -0,0 +1,21 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+
+        counts = Counter(nums)
+        return counts
+        
+     
+        
+
+
+        
+
+
+
+
+            
+
+        
+
+
+
